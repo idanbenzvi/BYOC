38de7e0506715375c58d95a8095df52055920fe4
Tal Kain <tal@kain.net>
2017-05-03 16:08:08 +0300

    fixed paths according to the linux env
    Signed-off-by: Tal Kain <tal@kain.net>

--- hw2/HW2_Src_for_sim_for_students-empty/SIM_HW2_filenames.vhd
+++ hw2/HW2_Src_for_sim_for_students-empty/SIM_HW2_filenames.vhd
@@ -16,8 +16,12 @@ use ieee.std_logic_1164.all;
 use std.textio.all;
 use IEEE.NUMERIC_STD.all;
 
-constant	program_in_file_name 	: string := "..\..\HW2_Src_for_sim_for_students\SIM_HW2_program.dat";
-constant	data_in_file_name 		: string := "..\..\HW2_Src_for_sim_for_students\SIM_HW2_TB_data.dat";
+--constant	program_in_file_name 	: string := "..\..\HW2_Src_for_sim_for_students\SIM_HW2_program.dat";
+--constant	data_in_file_name 		: string := "..\..\HW2_Src_for_sim_for_students\SIM_HW2_TB_data.dat";
+-- TODO: should we declare in any way that we changed the filenames to fit linux instead of windows?
+-- Paths for the filenames on Linux machine
+constant	program_in_file_name 	: string := "../../HW2_Src_for_sim_for_students\SIM_HW2_program.dat";
+constant	data_in_file_name 		: string := "../../HW2_Src_for_sim_for_students\SIM_HW2_TB_data.dat";
 
 signal 		no_hold					: STD_LOGIC := '0'; -- MUST be '0' for correct TB messages !!!
 
