b82c12879a088f9f546d4a9240eea98017c581b0
Tal Kain <tal@kain.net>
2017-05-07 16:47:02 +0300

    added all xilinx project files

---
+++ hw2/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/media/sf_shared_with_vm/byoc2017/projects/shared/hw2/HW2_top_isim_beh.exe" -prj "/media/sf_shared_with_vm/byoc2017/projects/shared/hw2/HW2_top_beh.prj" "work.HW2_top" 
