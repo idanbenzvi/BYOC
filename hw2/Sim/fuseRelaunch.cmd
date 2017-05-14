dffb3ecb69bfbca32c5609b8b7e6d84f969a60f6
Tal Kain <tal@kain.net>
2017-05-14 17:43:48 +0300

    changed directory structure according to the assignment
    Signed-off-by: Tal Kain <tal@kain.net>

---
+++ hw2/Sim/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/media/sf_shared_with_vm/byoc2017/projects/shared/hw2/Sim/BYOC_TB_for_students_isim_beh.exe" -prj "/media/sf_shared_with_vm/byoc2017/projects/shared/hw2/Sim/BYOC_TB_for_students_beh.prj" "work.BYOC_TB_for_students" 
