b1f0298c1359b38b30677008a4e6e6fe281faba2
Tal Kain <tal@kain.net>
2017-05-17 13:14:43 +0300

    updated the GPR filename file paths
    Signed-off-by: Tal Kain <tal@kain.net>

--- hw3/Src_4sim/SIM_HW3_GPR_filenames.vhd
+++ hw3/Src_4sim/SIM_HW3_GPR_filenames.vhd
@@ -13,7 +13,7 @@ use ieee.std_logic_unsigned.all ;
 use ieee.std_logic_1164.all ;
 use IEEE.NUMERIC_STD.all ;
 
-constant	GPR_data_in_file_name	 	: string := "..\..\HW3_GPR_File_src_for_sim\SIM_GPR_TB_data.dat";
+constant	GPR_data_in_file_name	 	: string := "../Src_4sim/SIM_GPR_TB_data.dat";
 
 signal 		no_hold					: STD_LOGIC :='0'; -- MUST be '0' for correct TB messages!!!
 
