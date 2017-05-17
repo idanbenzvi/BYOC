4c42bdd59c8e18ffb0c2addc7cc68dd886915bda
Tal Kain <tal@kain.net>
2017-05-17 15:08:24 +0300

    fixed the paths of the GPR filenames
    Signed-off-by: Tal Kain <tal@kain.net>

--- hw3/GPR_File/GPR_File_Src/SIM_HW3_GPR_filenames.vhd
+++ hw3/GPR_File/GPR_File_Src/SIM_HW3_GPR_filenames.vhd
@@ -14,7 +14,7 @@ use ieee.std_logic_1164.all ;
 use IEEE.NUMERIC_STD.all ;
 
 --constant	GPR_data_in_file_name	 	: string := "../Src_4sim/SIM_GPR_TB_data.dat";
-constant	GPR_data_in_file_name	 	: string := "/home/parallels/Documents/byoc/BYOC_Lab_Project_1/hw3/GPR_File/Src_4sim/SIM_GPR_TB_data.dat";
+constant	GPR_data_in_file_name	 	: string := "/home/parallels/Documents/byoc/BYOC_Lab_Project_1/hw3/GPR_File/GPR_File_Src/SIM_GPR_TB_data.dat";
 signal 		no_hold					: STD_LOGIC :='0'; -- MUST be '0' for correct TB messages!!!
 
 end BYOC_filenames ;
