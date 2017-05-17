261881df1cda047b4276317965d76904f59a7c32
Tal Kain <tal@kain.net>
2017-05-17 15:04:11 +0300

    updated the save step script
    Signed-off-by: Tal Kain <tal@kain.net>

--- hw3/save_step.sh
+++ hw3/save_step.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 FILENAME="hw3_`date | sed 's/[[:space:]]/_/g' | sed 's/:/_/g'`.zip"
-zip -r output/${FILENAME} GPR_File
+zip -r output/${FILENAME} GPR_File MIPS_ALU
 echo "Saved to ${FILENAME}"
 
 
