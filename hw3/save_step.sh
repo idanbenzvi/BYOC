0d288ee87eee0c142af84eec623c768d891011f2
Tal Kain <tal@kain.net>
2017-05-17 15:40:12 +0300

    added the disassembly to the save step script
    Signed-off-by: Tal Kain <tal@kain.net>

--- hw3/save_step.sh
+++ hw3/save_step.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 FILENAME="hw3_`date | sed 's/[[:space:]]/_/g' | sed 's/:/_/g'`.zip"
-zip -r output/${FILENAME} GPR_File MIPS_ALU
+zip -r output/${FILENAME} GPR_File MIPS_ALU Disassembly
 echo "Saved to ${FILENAME}"
 
 
