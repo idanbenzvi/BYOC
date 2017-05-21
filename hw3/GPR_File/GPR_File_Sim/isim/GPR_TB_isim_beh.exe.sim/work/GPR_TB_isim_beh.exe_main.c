8b2c97bcc9cb3fa4c15eadc0ac5e5fc5e71fe8fe
idan ben-zvi <idanbenzvi@gmail.com>
2017-05-21 22:47:18 +0300

    added docs files.
    Signed-off-by: idan ben-zvi <idanbenzvi@gmail.com>

--- hw3/GPR_File/GPR_File_Sim/isim/GPR_TB_isim_beh.exe.sim/work/GPR_TB_isim_beh.exe_main.c
+++ hw3/GPR_File/GPR_File_Sim/isim/GPR_TB_isim_beh.exe.sim/work/GPR_TB_isim_beh.exe_main.c
@@ -14,12 +14,12 @@
 
 struct XSI_INFO xsi_info;
 
-char *STD_TEXTIO;
+char *IEEE_P_1242562249;
 char *IEEE_P_2592010699;
 char *IEEE_P_3499444699;
 char *STD_STANDARD;
+char *STD_TEXTIO;
 char *IEEE_P_3620187407;
-char *IEEE_P_1242562249;
 char *WORK_P_2152067958;
 
 
@@ -42,13 +42,13 @@ int main(int argc, char **argv)
 
     xsi_register_tops("work_a_1950581036_2956508150");
 
-    STD_TEXTIO = xsi_get_engine_memory("std_textio");
+    IEEE_P_1242562249 = xsi_get_engine_memory("ieee_p_1242562249");
     IEEE_P_2592010699 = xsi_get_engine_memory("ieee_p_2592010699");
     xsi_register_ieee_std_logic_1164(IEEE_P_2592010699);
     IEEE_P_3499444699 = xsi_get_engine_memory("ieee_p_3499444699");
     STD_STANDARD = xsi_get_engine_memory("std_standard");
+    STD_TEXTIO = xsi_get_engine_memory("std_textio");
     IEEE_P_3620187407 = xsi_get_engine_memory("ieee_p_3620187407");
-    IEEE_P_1242562249 = xsi_get_engine_memory("ieee_p_1242562249");
     WORK_P_2152067958 = xsi_get_engine_memory("work_p_2152067958");
 
     return xsi_run_simulation(argc, argv);
