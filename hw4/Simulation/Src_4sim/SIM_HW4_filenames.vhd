92e61f2ee0357330d182ae5a40e73d37fee61bac
Tal Kain <tal@kain.net>
2017-05-24 10:02:50 +0300

    changed the filenames' paths to the data files
    Signed-off-by: Tal Kain <tal@kain.net>

--- hw4/Simulation/Src_4sim/SIM_HW4_filenames.vhd
+++ hw4/Simulation/Src_4sim/SIM_HW4_filenames.vhd
@@ -16,9 +16,11 @@ use ieee.std_logic_1164.all;
 use std.textio.all;
 use IEEE.NUMERIC_STD.all;
 
-constant	program_in_file_name 	: string := "..\..\HW4_Src_for_sim_for_students\SIM_HW4_program.dat";
-constant	data_in_file_name 		: string := "..\..\HW4_Src_for_sim_for_students\SIM_HW4_TB_data.dat";
+--constant	program_in_file_name 	: string := "..\..\HW4_Src_for_sim_for_students\SIM_HW4_program.dat";
+--constant	data_in_file_name 		: string := "..\..\HW4_Src_for_sim_for_students\SIM_HW4_TB_data.dat";
 
+constant	program_in_file_name 	: string := "/home/parallels/Documents/byoc/BYOC_Lab_Project_1/hw4/Simulation/Src_4sim/SIM_HW4_program.dat";
+constant	data_in_file_name 		: string := "/home/parallels/Documents/byoc/BYOC_Lab_Project_1/hw4/Simulation/Src_4sim/SIM_HW4_TB_data.dat";
 signal 		no_hold					: STD_LOGIC := '0'; -- MUST be '0' for correct TB messages !!!
 
 end BYOC_filenames;
