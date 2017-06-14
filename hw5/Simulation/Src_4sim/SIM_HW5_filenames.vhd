38d15f507007be022fd3f421eb614bd526b41ea8
Tal Kain <tal@kain.net>
2017-06-14 12:06:56 +0300

    fixed the filenames of HW5
    Signed-off-by: Tal Kain <tal@kain.net>

--- hw5/Simulation/Src_4sim/SIM_HW5_filenames.vhd
+++ hw5/Simulation/Src_4sim/SIM_HW5_filenames.vhd
@@ -16,8 +16,8 @@ use ieee.std_logic_1164.all;
 use std.textio.all;
 use IEEE.NUMERIC_STD.all;
 
-constant	program_in_file_name 	: string := "..\..\HW5_Src_for_sim_for_students\SIM_HW5_program.dat";
-constant	data_in_file_name 		: string := "..\..\HW5_Src_for_sim_for_students\SIM_HW5_TB_data.dat";
+constant	program_in_file_name 	: string := "/home/parallels/Documents/byoc/BYOC_Lab_Project_1/hw5/Simulation/Src_4sim/SIM_HW5_program.dat";
+constant	data_in_file_name 		: string := "/home/parallels/Documents/byoc/BYOC_Lab_Project_1/hw5/Simulation/Src_4sim/SIM_HW5_TB_data.dat";
 
 signal 		no_hold					: STD_LOGIC := '0'; -- MUST be '0' for correct TB messages !!!
 
