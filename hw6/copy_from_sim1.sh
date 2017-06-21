fe35d6154b76dc8338f990f870885b15cfb80a0f
Tal Kain <tal@kain.net>
2017-06-21 11:21:47 +0300

    added the copy_from_sim1.sh and get_files_from_hw5.sh scripts
    Signed-off-by: Tal Kain <tal@kain.net>

---
+++ hw6/copy_from_sim1.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cp Simulation1/Src_4sim/* Simulation2/Src_4sim/
+cp Simulation1/Sim/* Simulation2/Sim/
+cp Simulation1/Src_4sim/* Simulation3/Src_4sim/
+cp Simulation1/Sim/* Simulation3/Sim/
+echo Done
+
---
+++ hw6/get_files_from_hw5.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cp ../hw5/Simulation/Src_4sim/{Fetch_Unit.vhd,MIPS_ALU.vhd,GPR.vhd,dual_port_memory.vhd} Simulation1/Src_4sim/
+echo Done
+
